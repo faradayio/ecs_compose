33ceada9978d2e67f48a5dabd2500b4c84d78d8c
Eric Kidd <git@randomhacks.net>
2015-07-31 05:59:13 -0400

    Generate gem skeleton with bundler

---
+++ lib/ecs_compose.rb
@@ -0,0 +1,5 @@
+require "ecs_compose/version"
+
+module EcsCompose
+  # Your code goes here...
+end
---
+++ spec/ecs_compose_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe EcsCompose do
+  it 'has a version number' do
+    expect(EcsCompose::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
