bdaf1c003b8e54343b21dc3a0fece6c3a04fc05f
Eric Kidd <git@randomhacks.net>
2015-08-04 08:15:07 -0400

    Allow updating multiple ECS services from a manifest
    We include a fair bit of CLI-parsing code to make it easy to work with
    both standalone `docker-compose.yml` files and manifests with multiple
    services, and to preserve `docker-compose`-like command line conventions
    as much as possible.
    
    Basically, we want this to be a good tool, one which we use heavily for a
    variety of tasks, and one which offers an easy migration path for people
    familiar with `docker-compose`.

--- lib/ecs_compose/cli.rb
+++ lib/ecs_compose/cli.rb
@@ -4,19 +4,100 @@ require "thor"
 module EcsCompose
   # Our basic command-line interface.
   class CLI < Thor
-    desc("conv FAMILY [YAML_FILE]",
-         "Convert docker-compose.yml to ECS JSON format")
-    def jsonify(family, yaml_file="docker-compose.yml")
-      yaml = File.read(yaml_file)
-      puts EcsCompose::JsonGenerator.new(family, yaml).json
+    DEFAULT_FILE = "docker-compose.yml"
+    DEFAULT_MANIFEST = "deploy/DEPLOY-MANIFEST.yml"
+
+    class_option(:manifest, type: :string,
+                 aliases: %w(-m),
+                 desc: "Manifest describing a set of tasks and services (takes precedence over --file) [default: #{DEFAULT_MANIFEST}]")
+    class_option(:file, type: :string,
+                 aliases: %w(-f),
+                 desc: "File describing a single task or service [default: #{DEFAULT_FILE}]")
+    class_option(:file_info, type: :string,
+                 aliases: %w(-i),
+                 desc: "Type and name for use with --file [ex: 'service:hello' or 'task:migrate']")
+
+    desc("json [TASK_DEFINITION]",
+         "Convert a task definition to ECS JSON format")
+    def json(task_definition=nil)
+      if task_definition.nil?
+        choices = manifest.task_definitions.map {|td| td.name }
+        case choices.length
+        when 0
+          fatal_err("Please supply a manifest with at least one task definition")
+        when 1
+          task_definition = choices.first
+        else
+          fatal_err("Please choose one of: #{choices.join(', ')}")
+        end
+      end
+
+      found = manifest.task_definitions.find {|td| td.name } or
+        fatal_err("Can't find task definition: #{task_definition}")
+      puts EcsCompose::JsonGenerator.new(found.name, found.yaml).json
     end
 
-    desc("up SERVICE [YAML_FILE]",
-         "Update an ECS service to match YAML_FILE")
-    def up(service, yaml_file="docker-compose.yml")
-      yaml = File.read(yaml_file)
-      json = EcsCompose::JsonGenerator.new(service, yaml).json
-      EcsCompose::Ecs.update_service_with_json(service, json)
+    desc("up [SERVICES...]", "Update ECS services")
+    def up(*services)
+      available = manifest.task_definitions.select {|td| td.type == :service }
+      chosen =
+        if available.empty?
+          available
+        else
+          available.select {|td| services.include?(td.name) }
+        end
+
+      chosen.each do |service|
+        json = EcsCompose::JsonGenerator.new(service.name, service.yaml).json
+        EcsCompose::Ecs.update_service_with_json(service.name, json)
+      end
+    end
+
+    protected
+
+    # Figure out whether we have a manifest or a docker-compose.yml.  We
+    # check supplied flags first, then defaults, and we prefer manifests
+    # when there's a tie.
+    def mode
+      @mode ||=
+        if options.manifest
+          :manifest
+        elsif options.file
+          :file
+        elsif File.exist?(DEFAULT_MANIFEST)
+          :manifest
+        elsif File.exist?(DEFAULT_FILE)
+          :file
+        else
+          fatal_err("Unable to find either #{DEFAULT_FILE} or #{DEFAULT_MANIFEST}")
+        end
+    end
+
+    # Create a manifest, either by reading it in, or synthesizing it from a
+    # `docker-compose.yml` file and some extra arguments.
+    def manifest
+      @manifest ||=
+        case mode
+        when :manifest
+          Manifest.read_from_manifest(options.manifest || DEFAULT_MANIFEST)
+        when :file
+          info = options.file_info
+          if info.nil?
+            fatal_err("Must pass -i option when using docker-compose.yml")
+          end
+          unless info =~ /\A(service|task):[-_A-Za-z0-9\z]/
+            fatal_err("Incorrectly formatted -i option")
+          end
+          Manifest.read_from_file(options.file || DEFAULT_FILE,
+                                  type.to_sym, name)
+        else raise "Unknown mode: #{mode}"
+        end
+    end
+
+    # Print an error and quit.
+    def fatal_err(msg)
+      STDERR.puts(msg.red)
+      exit(1)
     end
   end
 end
