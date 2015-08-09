be17b32a947628632de1940e0a9ac32d8558c297
Eric Kidd <git@randomhacks.net>
2015-08-08 21:26:39 -0400

    Convert to docopt.rs
    Thor, GLI and Commander were all hopelessly unable to implement anything
    that looked like docker-compose's `run` command, because none of them
    had decent handling of repeated arguments, and few of them had any hooks
    to leave particular arguments unparsed.
    
    So I decided to try docopt, which is a cross-language standard with
    extensive conformance tests, and which handles subcommands and a few
    other useful cases.  Unforunately, Ruby's docopt.rb is much more limited
    than docopt in other languages.  However, with enough poking and
    prodding, it can be made to behave in an _almost_ reasonable fashion.
    
    Somebody needs to look at git, bundler, docker-compose, etc., and
    actually write a Ruby gem which covers all the argument-parsing cases.
    Or Ruby's docopt implementation needs to be enhanced to cover the `git`
    example from Python docopt.

--- lib/ecs_compose/task_definition.rb
+++ lib/ecs_compose/task_definition.rb
@@ -1,13 +1,52 @@
 module EcsCompose
 
-  # Information required to create an ECS task definition.
+  # Information required to create an ECS task definition, and commands to
+  # act upon it.
   class TaskDefinition
     attr_reader :type, :name, :yaml
 
+    # Create a new TaskDefinition.  The type should be either `:task` or
+    # `:service`, the name should be both the name of the ECS task
+    # definition and the corresponding ECS service (if any), and the YAML
+    # should be `docker-compose`-format YAML describing the containers
+    # associated with this task.
     def initialize(type, name, yaml)
       @name = name
       @type = type
       @yaml = yaml
+    end
+
+    # Register this task definition with ECS.  Will create the task
+    # definition if it doesn't exist, and add a new version of the task.
+    def register
+      EcsCompose::Ecs.register_task_definition(to_json)
+    end
+
+    # Register this task definition with ECS, and update the corresponding
+    # service.
+    def up
+      EcsCompose::Ecs.update_service_with_json(name, to_json)
+    end
+
+    # Run this task definition as a one-shot ECS task, with the specified
+    # overrides.
+    def run(environment: {}, entrypoint: nil, command: nil)
+      puts "environment: #{environment.inspect}"
+      puts "entrypoint: #{entrypoint.inspect}"
+      puts "command: #{command.inspect}"
+      raise "Not yet implemented"
+    end
+
+    # Generate ECS task definition JSON for this instance.
+    def to_json
+      json_generator.json
+    end
+
+    protected
+
+    # Return a JSON generator for this task.
+    def json_generator
+      @json_generator ||= EcsCompose::JsonGenerator.new(name, yaml)
     end
   end
 end
