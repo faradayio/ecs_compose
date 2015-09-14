7ebd754d026f02e463fd240d330c303ce7227c76
Eric Kidd <git@randomhacks.net>
2015-09-14 13:11:36 -0400

    Add support for docker-compose-like `--project-name`
    Right now, the only way to specify a prefix for all service names is to
    generate it as part of a the manifest.  But this means that it's
    impossible to run multiple copies of the same manifest under different
    names.
    
    This patch allows us to set a prefix for all task definition and service
    names, turning "foo" into "staging-foo".

--- lib/ecs_compose/manifest.rb
+++ lib/ecs_compose/manifest.rb
@@ -4,22 +4,37 @@ module EcsCompose
 
   # A collection of multiple task definitions, including names and types.
   class Manifest
-    # Build a manifest from a single `docker-compose.yml` file, using the
-    # supplied type and name.
-    def self.read_from_file(path, type, name)
-      new([TaskDefinition.new(type, name, File.read(path))])
-    end
-
-    # Read in a complete manifest.
-    def self.read_from_manifest(path)
-      dir = File.dirname(path)
-      yaml = Psych.load_file(path)
-      defs = yaml.fetch('task_definitions').map do |name, info|
-        TaskDefinition.new(info.fetch('type').to_sym,
-                           name,
-                           File.read(File.join(dir, info.fetch('path'))))
+    class << self
+      # Build a manifest from a single `docker-compose.yml` file, using the
+      # supplied type and name.
+      def read_from_file(path, project_name, type, name)
+        new([TaskDefinition.new(type,
+                                compound_name(project_name, name),
+                                File.read(path))])
       end
-      new(defs)
+
+      # Read in a complete manifest.
+      def read_from_manifest(path, project_name)
+        dir = File.dirname(path)
+        yaml = Psych.load_file(path)
+        defs = yaml.fetch('task_definitions').map do |name, info|
+          TaskDefinition.new(info.fetch('type').to_sym,
+                             compound_name(project_name, name),
+                             File.read(File.join(dir, info.fetch('path'))))
+        end
+        new(defs)
+      end
+
+      private
+
+      # If we've been supplied with a `project_name`, build a compound name.
+      def compound_name(project_name, name)
+        if project_name.nil?
+          name
+        else
+          "#{project_name}-#{name}"
+        end
+      end
     end
 
     attr_reader :task_definitions
--- spec/manifest_spec.rb
+++ spec/manifest_spec.rb
@@ -5,13 +5,13 @@ describe EcsCompose::Manifest do
   describe ".read_from_file" do
     let(:file) { fixture_path("deploy/hello.yml") }
 
-    subject { EcsCompose::Manifest.read_from_file(file, :service, 'hello') }
+    subject { EcsCompose::Manifest.read_from_file(file, 'test', :service, 'hello') }
 
     it "loads a single task definition with the supplied type and name" do
       defs = subject.task_definitions
       expect(defs.length).to eq(1)
       expect(defs[0].type).to eq(:service)
-      expect(defs[0].name).to eq('hello')
+      expect(defs[0].name).to eq('test-hello')
       expect(defs[0].yaml).to eq(File.read(file))
     end
   end
@@ -19,7 +19,7 @@ describe EcsCompose::Manifest do
   describe ".read_from_manifest" do
     let(:manifest) { fixture_path("deploy/DEPLOY-MANIFEST.yml") }
 
-    subject { EcsCompose::Manifest.read_from_manifest(manifest) }
+    subject { EcsCompose::Manifest.read_from_manifest(manifest, 'test') }
 
     it "loads all task definitions from the manifest" do
       defs = subject.task_definitions
@@ -27,12 +27,12 @@ describe EcsCompose::Manifest do
 
       hello = defs[0]
       expect(hello.type).to eq(:service)
-      expect(hello.name).to eq('hello')
+      expect(hello.name).to eq('test-hello')
       expect(hello.yaml).to eq(File.read(fixture_path("deploy/hello.yml")))
 
       hellocli = defs[1]
       expect(hellocli.type).to eq(:task)
-      expect(hellocli.name).to eq('hellocli')
+      expect(hellocli.name).to eq('test-hellocli')
       expect(hellocli.yaml).to eq(File.read(fixture_path("deploy/hellocli.yml")))
     end
   end
