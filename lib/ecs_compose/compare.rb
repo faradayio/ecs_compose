84c492e7d09de5b90b7c77d2864f7ff9d946b7f8
Eric Kidd <git@randomhacks.net>
2015-12-09 12:17:24 -0500

    [idempotent] Normalize and compare task definitions

--- lib/ecs_compose/compare.rb
+++ lib/ecs_compose/compare.rb
@@ -1,20 +1,44 @@
 module EcsCompose
+  # Utilities for comparing ECS task definition JSON, which we need to do
+  # to determine whether or not a running service needs to be deployed.
   module Compare
-    def self.sort_recursively!(val)
+
+    # Recursively sort all arrays contained in `val`, in order to normalize
+    # things like environment variables in different orders.
+    def self.sort_recursively(val)
       case val
       when Array
-        val.sort_by! do |item|
+        val.sort_by do |item|
           if item.instance_of?(Hash)
             item.to_a.sort
           else 
             item
           end
-        end
-        val.each {|item| sort_recursively!(item) }
+        end.map {|item| sort_recursively(item) }
       when Hash
-        val.values.each {|item| sort_recursively!(item) }
+        newval = {}
+        val.each do |k, v|
+          newval[k] = sort_recursively(v)
+        end
+        newval
+      else
+        val
       end
-      val
+    end
+
+    # Do two task definitions match after normalization?
+    def self.task_definitions_match?(td1, td2)
+      normalize_task_definition(td1) == normalize_task_definition(td2)
+    end
+
+    protected
+
+    # Sort and strip out things we expect to change.
+    def self.normalize_task_definition(td)
+      td = sort_recursively(td)
+      td.delete("taskDefinitionArn")
+      td.delete("revision")
+      td
     end
   end
 end
--- spec/compare_spec.rb
+++ spec/compare_spec.rb
@@ -1,11 +1,28 @@
 require 'spec_helper'
+require 'json'
 
 describe EcsCompose::Compare do
   describe ".sort_recursively!" do
     it "sorts lists recursively, including lists of hashes" do
       data = {a: [{b:[2]}, {b:[1]}]}
-      EcsCompose::Compare.sort_recursively!(data)
-      expect(data).to eq({a: [{b:[1]}, {b:[2]}]})
+      sorted = EcsCompose::Compare.sort_recursively(data)
+      expect(data).to eq({a: [{b:[2]}, {b:[1]}]})
+      expect(sorted).to eq({a: [{b:[1]}, {b:[2]}]})
+    end
+  end
+
+  describe ".task_definitions_match?" do
+    def load_taskdef(id)
+      JSON.parse(File.read(fixture_path("taskdef#{id}.json")))
+        .fetch("taskDefinition")
+    end
+
+    let(:taskdef14) { load_taskdef(14) }
+    let(:taskdef15) { load_taskdef(15) }
+
+    it "normalizes ARNs and list orders" do
+      result = EcsCompose::Compare.task_definitions_match?(taskdef14, taskdef15)
+      expect(result).to eq(true)
     end
   end
 end
