baefd0c65d35f67091c464c3ef4026e6306b3632
Eric Kidd <git@randomhacks.net>
2015-12-17 08:38:10 -0500

    Fix JSON normalization
    We keep encountering edge cases in the JSON normalization thanks to the
    difficulty of sorting mixed data types with Ruby.  Rather than continue
    to mess with this, I'm proposing a comprehensive fix with extensive
    tests.  We define a new `compare_any` function that works like `<=>` but
    which is defined to compare any two JSON values.
    
    We test this providing many examples of how different pairs of values
    should be ordered, and verifying the `compare_any` handles each the way
    we want.  Then, using this improved comparison primitive, we rebuild
    `sort_recursively`.
    
    I've merged in the extra `sort_recursively` tests provided by Derek and
    Seamus.
    
    With luck, this should handle all the different JSON we need to
    normalize for comparison.  And if there are still problematic cases,
    well, we have a much stronger base to build on.

--- lib/ecs_compose/compare.rb
+++ lib/ecs_compose/compare.rb
@@ -2,19 +2,59 @@ module EcsCompose
   # Utilities for comparing ECS task definition JSON, which we need to do
   # to determine whether or not a running service needs to be deployed.
   module Compare
+    # :nodoc: An internal table providing a sort order for data types.
+    CLASS_ORDINAL = {
+      String => 1,
+      # These can all be compared against each other.
+      Fixnum => 2, Float => 2, Bignum => 2,
+      Hash => 3,
+      Array => 4,
+      FalseClass => 5,
+      TrueClass => 6,
+      NilClass => 7,
+    }
+
+    # Compare two legal JSON values, and do our best to always return a
+    # result.
+    def self.compare_any(a, b)
+      # First sort by type.
+      class_order =
+        CLASS_ORDINAL.fetch(a.class) <=> CLASS_ORDINAL.fetch(b.class)
+      return class_order unless class_order == 0
+
+      # Then sort by value.
+      case a
+      when Hash
+        # Convert the hashes to arrays, sort them to normalize hash key
+        # order, and compare them.
+        compare_any(a.to_a.sort {|a1, b1| compare_any(a1, b1) },
+                    b.to_a.sort {|a1, b1| compare_any(a1, b1) })
+      when Array
+        # Do a full array comparison so that we can slip in our comparison
+        # function.
+        for i in 0...([a.length, b.length].max)
+          if i >= a.length
+            return -1
+          elsif i >= b.length
+            return 1
+          else
+            elem_order = compare_any(a[i], b[i])
+            return elem_order unless elem_order == 0
+          end
+        end
+        0
+      else
+        a <=> b
+      end
+    end
 
     # Recursively sort all arrays contained in `val`, in order to normalize
     # things like environment variables in different orders.
     def self.sort_recursively(val)
       case val
       when Array
-        val.sort_by do |item|
-          if item.instance_of?(Hash)
-            item.to_a.sort
-          else 
-            item
-          end
-        end.map {|item| sort_recursively(item) }
+        val.map {|item| sort_recursively(item) }
+          .sort {|a1, b1| compare_any(a1, b1) }
       when Hash
         newval = {}
         val.each do |k, v|
--- spec/compare_spec.rb
+++ spec/compare_spec.rb
@@ -2,12 +2,39 @@ require 'spec_helper'
 require 'json'
 
 describe EcsCompose::Compare do
+  describe ".compare_any" do
+    it "permits comparison of any type which may appear in JSON" do
+      # According to http://www.json.org/, valid JSON types are: string
+      # number object array true false nil.  This list should be a set of
+      # legal JSON values sorted from smallest to largest.
+      values = ["a", "b", 1, 1.5, 2,
+                {"a" => 1}, {"a" => 2}, {"b" => 1},
+                {"d" => 2, "c" => 1}, {"d" => 1, "c" => 2},
+                {"e" => 1}, {"e" => nil},
+                [], [1], [1,1], [1,2], [2], [2,1], [nil], false, true, nil]
+      for i in 0...values.length
+        for j in 0...values.length
+          puts "#{values[i].inspect} <=> #{values[j].inspect}"
+          expect(EcsCompose::Compare.compare_any(values[i], values[j]))
+            .to eq((i-j) <=> 0)
+        end
+      end
+    end
+  end
+
   describe ".sort_recursively!" do
     it "sorts lists recursively, including lists of hashes" do
-      data = {a: [{b:[2]}, {b:[1]}]}
+      data = {"a" => [{"b" => [2]}, {"b" => [1]}]}
       sorted = EcsCompose::Compare.sort_recursively(data)
-      expect(data).to eq({a: [{b:[2]}, {b:[1]}]})
-      expect(sorted).to eq({a: [{b:[1]}, {b:[2]}]})
+      expect(sorted).to eq({"a" => [{"b" => [1]}, {"b" => [2]}]})
+      # Ensure no mutation.
+      expect(data).to eq({"a" => [{"b" => [2]}, {"b" => [1]}]})
+    end
+
+    it 'sorts arrays of arrays', :focus do
+      data = [[[],[]], [3,4], [1,2], [5, 6]]
+      sorted = EcsCompose::Compare.sort_recursively(data)
+      expect(sorted).to eq([[1,2], [3,4], [5, 6], [[],[]]])
     end
   end
 
